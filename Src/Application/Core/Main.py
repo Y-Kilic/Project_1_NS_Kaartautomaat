03872a7fe3c9d6732bb874dd740e8249d91adef9
Y-Kilic <CyanGithub@protonmail.com>
2019-10-29 23:01:05 +0100

    Main in parent mapje geplaats zodat modules beter werken.

--- Src/Application/Core/Main.py
+++
@@ -1,4 +0,0 @@
-from Src.Application.API.NS_API import NS_API
-test1 = NS_API
-
-print(test1.getDepartures("UT", 10))
---
+++ Src/Application/Main.py
@@ -0,0 +1,7 @@
+from API.NS_API import NS_API
+
+test1 = NS_API
+
+print(test1.getDepartures("UT", 10))
+
+result = input("")
