a6aadd5532b25c980aebe20cc4b3d64a48ffa9d0
Y-Kilic <CyanGithub@protonmail.com>
2019-10-29 17:28:09 +0100

    Algemeen reisinformatie methode toegevoegd.

--- Src/Application/API/NS_API.py
+++ Src/Application/API/NS_API.py
@@ -1,4 +1,25 @@
 class NS_API(object):
     """description of class"""
 
+    key = "5404c0d6f7b64a6cb3fea06d314058b7"
+    import http.client, urllib.parse, json
 
+    def generalTravelInfo(station):
+        params = urllib.parse.urlencode({
+        'maxJourneys': '25',
+        'station': str(station)
+        })
+
+        try:
+            conn = http.client.HTTPSConnection('gateway.apiportal.ns.nl')
+            conn.request("GET", "/public-reisinformatie/api/v2/departures?" + params, headers=key)
+
+            response = conn.getresponse()
+            responsetext = response.read()
+            data = json.loads(responsetext)
+   
+            return data
+            conn.close()
+
+        except Exception as e:
+            print("Fout: {} {}".format(e.errno, e.strerror))
