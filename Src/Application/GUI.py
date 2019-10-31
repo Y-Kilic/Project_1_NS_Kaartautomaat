219a146a774087800bee2c6f264d7edf475ea178
Thijs <thijs.breukelaar@gmail.com>
2019-10-31 17:25:36 +0100

    fixed delays

---
+++ Src/Application/GUI.py
@@ -0,0 +1,113 @@
+from Services.InfoService import InfoService
+from Managers.InfoManager import InfoManager
+from tkinter import *
+from tkinter import messagebox
+import datetime
+
+infoService = InfoService
+departures = infoService.generalTravelInfo()
+
+##### D U M M Y   V A R I A B L E S #####
+allStations = {"Utrecht Centraal", "Amsterdam Centraal"}
+#delay = '_ min'
+##### D U M M Y   V A R I A B L E S #####
+
+# GUI list of departures
+def show_departures(selectedStation):
+    GUImenu.pack_forget()
+    GUIdepartures.pack()
+    root.title("Actuele reisinformatie voor: " + str(selectedStation))
+    Label(master=GUIdepartures, text=("Actuele reisinformatie voor: " + str(selectedStation)), font=buttonFont, foreground=nsBlue, background=nsYellow).place(x=300, y=20)
+
+    Label(master=GUIdepartures, text="Geplande \nvertrektijd", font=buttonFont, foreground=nsBlueDark, background=nsYellow).place(x=120, y=47)
+    Label(master=GUIdepartures, text='Richting', font=buttonFont, foreground=nsBlueDark, background=nsYellow).place(x=230, y=65)
+    Label(master=GUIdepartures, text='Spoor', font=buttonFont, foreground=nsBlueDark, background=nsYellow).place(x=360, y=65)
+    Label(master=GUIdepartures, text='Type', font=buttonFont, foreground=nsBlueDark, background=nsYellow).place(x=430, y=65)
+    Label(master=GUIdepartures, text='Vertraging', font=buttonFont, foreground=nsBlueDark, background=nsYellow).place(x=520, y=65)
+    Label(master=GUIdepartures, text='Verwachte \nvertrektijd', font=buttonFont, foreground=nsBlueDark, background=nsYellow).place(x=660, y=47)
+
+    yaxis = 100
+    for departure in departures[:16]:
+        delay = InfoManager.getDelay(departure['plannedDateTime'], departure['actualDateTime'])
+
+        Label(master=GUIdepartures, text=departure['plannedDateTime'][11:16], font=credFont, foreground=nsBlueDark, background=nsYellow).place(x=160, y=yaxis)
+        Label(master=GUIdepartures, text=departure['direction'], font=credFont, foreground=nsBlueDark, background=nsYellow).place(x=230, y=yaxis)
+        Label(master=GUIdepartures, text=departure['plannedTrack'], font=credFont, foreground=nsBlueDark, background=nsYellow).place(x=375, y=yaxis)
+        Label(master=GUIdepartures, text=departure['trainCategory'], font=credFont, foreground=nsBlueDark, background=nsYellow).place(x=445, y=yaxis)
+        Label(master=GUIdepartures, text=delay, font=credFont, foreground=nsBlueDark, background=nsYellow).place(x=525, y=yaxis)
+        Label(master=GUIdepartures, text=departure['actualDateTime'][11:16], font=credFont, foreground=nsBlueDark, background=nsYellow).place(x=670, y=yaxis)
+        yaxis += 25
+
+# GUI station validator
+def station_validator(station):
+    selectedStation = station
+    if selectedStation not in allStations:
+        messagebox.showinfo("Foutmelding", "Geen station gevonden, probeert u het alstublieft opnieuw.")
+    else:
+        show_departures(selectedStation)
+
+# GUI unavailable functions pop-up
+def out_of_order():
+    messagebox.showinfo("Foutmelding", "Deze functie is tijdelijk niet beschikbaar. Excuses voor het ongemak!")
+
+# GUI station selection
+def select_station():
+    searchBar.place(x=600, y=390)
+    Label(master=GUImenu, text="Van welk station \nwilt u de vertrekinformatie?", font=buttonFont, foreground=nsBlueDark, background=nsYellow).place(x=555, y=340)
+    Label(master=GUImenu, text="bijv. 'Utrecht Centraal'", font=credFont, foreground=nsBlueDark, background=nsYellow).place(x=600, y=420)
+    Button(master=GUImenu, text="Zoek", font=credFont, foreground=nsWhite, background=nsBlue, command=lambda: station_validator(searchBar.get())).place(x=730, y=386)
+
+# GUI main menu function
+def main_menu():
+    GUIdepartures.pack_forget()
+    GUImenu.pack()
+    root.title("NS Kaartautomaat")
+
+# GUI fonts & colours
+splashFont = ("Helvetica", 48, "bold")
+buttonFont = ("Helvetica", 12, "bold")
+credFont = ("Helvetica", 10)
+
+nsBlue = "#0000A5"      # Buttons
+nsBlueDark = "#002C8C"  # Splash text
+nsYellow = "#FFC800"    # Background
+nsWhite = "#FFFFFF"     # Button text
+nsRed = "FF0000"        # Delayed or cancelled
+
+# GUI setup
+root = Tk()
+
+# GUI main menu frame
+GUImenu = Frame(master=root)
+GUImenu.configure(height=700, width=940, background=nsYellow)
+
+Label(master=GUImenu, text="Welkom bij NS", font=splashFont, foreground=nsBlueDark, background=nsYellow).place(x=250, y=100)
+Label(master=GUImenu, text="By: Annastazia, Thijs, Yaser & Yasin", font=credFont, foreground=nsBlueDark, background=nsYellow).place(x=700, y=670)
+
+buttonAmsterdam = Button(master=GUImenu, text="Ik wil naar \nAmsterdam", foreground=nsWhite, background=nsBlue, font=buttonFont, command=out_of_order, height=3, width=15).place(x=120, y=250)
+buttonKaartje = Button(master=GUImenu, text="Kopen \nlos kaartje", foreground=nsWhite, background=nsBlue, font=buttonFont, command=out_of_order, height=3, width=15).place(x=300, y=340)
+buttonOV = Button(master=GUImenu, text="Kopen \n OV-chipkaart", foreground=nsWhite, background=nsBlue, font=buttonFont, command=out_of_order, height=3, width=15).place(x=120, y=340)
+buttonBuitenland = Button(master=GUImenu, text="Ik wil naar \nhet buitenland", foreground=nsWhite, background=nsBlue, font=buttonFont, command=out_of_order, height=3, width=15).place(x=300, y=250)
+buttonDepartures = Button(master=GUImenu, text="Opzoeken \n vertrektijden", foreground=nsWhite, background=nsBlue, font=buttonFont, command=select_station, height=3, width=33).place(x=120, y=450)
+
+searchBar = Entry(master=GUImenu)
+
+# GUI departures frame
+GUIdepartures = Frame(master=root)
+GUIdepartures.configure(height=700, width=940, background=nsYellow)
+
+Button(master=GUIdepartures, text="< Terug naar menu", font=buttonFont, foreground=nsWhite, background=nsBlue, command=main_menu).place(x=10, y=10)
+
+
+
+main_menu()
+root.mainloop()
+
+#for departure in result['departures']:
+#    ## laat zien welke attributes in per departure zit.
+#    print("###########################################################")
+#    print(departure.keys())
+#    print("###########################################################")
+#    ## dus je kan nu variables gebruiken om te krijgen wat je wilt bv
+#    print(f"Departure naam is: {departure['name']}")
+#    print(f"Departure tijd is: {departure['plannedDateTime']}")
--- Src/Application/Managers/InfoManager.py
+++ Src/Application/Managers/InfoManager.py
@@ -1,8 +1,15 @@
+import datetime
+
 class InfoManager(object):
     def getDelay(plannedDateTime, actualDateTime):
         if plannedDateTime == actualDateTime:
-            return "-"
-
-        delay = plannedDateTime - actualDateTime
+            delay = "-"
+            return delay
+        planned = plannedDateTime.replace('T', ':')
+        actual = actualDateTime.replace('T', ':')
+        planned = datetime.datetime.strptime(planned[11:16], '%H:%M')
+        actual = datetime.datetime.strptime(actual[11:16], '%H:%M')
+        delay = actual - planned
+        return str(delay)
 
 
